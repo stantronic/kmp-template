605b9dbd83b3da19c2ca9eb949b2b7f8f87dce61
Stanton <stantronic@gmail.com>
2025-08-09 14:31:09 +0100

    Fix imports

--- composeApp/src/commonMain/kotlin/space/stanton/starter/App.kt
+++ composeApp/src/commonMain/kotlin/space/stanton/starter/App.kt
@@ -13,11 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
